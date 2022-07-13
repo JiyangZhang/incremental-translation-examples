ffffda1341f3b831d1b11de6749bfc92b5628c19
JiyangZhang <jiyang.zhang@utexas.edu>
2022-07-13 16:58:20 -0500

    Csharp old version:Tue Dec 3 12:04:21 2019

---
+++ CreateBucket.cs
@@ -0,0 +1,34 @@
+﻿class StorageQuickstart
+{
+    static void Main(string[] args)
+    {
+        // Your Google Cloud Platform project ID.
+        string projectId = "YOURPROJECTID";
+
+        // [END storage_quickstart]
+        Debug.Assert("YOURPROJECT" + "ID" != projectId,
+            "Edit Program.cs and replace YOURPROJECTID with your Google Project Id.");
+        // [START storage_quickstart]
+
+        // Instantiates a client.
+        using (StorageClient storageClient = StorageClient.Create())
+        {
+            // The name for the new bucket.
+            string bucketName = projectId + "testbucket";
+            try
+            {
+                // Creates the new bucket.
+                storageClient.CreateBucket(projectId, bucketName);
+                Console.WriteLine($"Bucket {bucketName} created.");
+            }
+            catch (Google.GoogleApiException e)
+            when (e.Error.Code == 409)
+            {
+                // The bucket already exists.  That's fine.
+                Console.WriteLine(e.Error.Message);
+            }
+        }
+    }
+}
+
+
